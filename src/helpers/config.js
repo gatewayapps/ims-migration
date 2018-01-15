5744cde26ff7d4187da023de23da3e93a3f6f878
Justin Neff <justin@gatewayapps.com>
2018-01-15 07:59:05 -0600

    Fix eslint errors

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -2,18 +2,18 @@ import path from 'path'
 import fs from 'fs-extra'
 import yaml from 'js-yaml'
 
-export function loadConfig(configPath) {
+export function loadConfig (configPath) {
   configPath = configPath || 'migration.yaml'
   const configFile = path.resolve(configPath)
   return resolvePaths(yaml.safeLoad(fs.readFileSync(configFile)), configFile)
 }
 
-export function getTypePath(pathType) {
+export function getTypePath (pathType) {
   const config = loadConfig()
   return config.paths[pathType]
 }
 
-export function addPreDeployScript(scriptName) {
+export function addPreDeployScript (scriptName) {
   const config = loadConfig()
   if (!Array.isArray(config.preDeploy)) {
     config.preDeploy = [scriptName]
@@ -23,7 +23,7 @@ export function addPreDeployScript(scriptName) {
   writeConfig(config)
 }
 
-export function addPostDeployScript(scriptName) {
+export function addPostDeployScript (scriptName) {
   const config = loadConfig()
   if (!Array.isArray(config.postDeploy)) {
     config.postDeploy = [scriptName]
@@ -33,7 +33,7 @@ export function addPostDeployScript(scriptName) {
   writeConfig(config)
 }
 
-export function addMigration(scriptName) {
+export function addMigration (scriptName) {
   const config = loadConfig()
   if (!Array.isArray(config.migrations)) {
     config.migrations = [scriptName]
@@ -43,12 +43,12 @@ export function addMigration(scriptName) {
   writeConfig(config)
 }
 
-function writeConfig(config) {
+function writeConfig (config) {
   const configFile = path.resolve('migration.yaml')
   fs.writeFileSync(configFile, yaml.safeDump(unresolvePaths(config, configFile)))
 }
 
-function resolvePaths(migrationConfig, migrationFilePath) {
+function resolvePaths (migrationConfig, migrationFilePath) {
   const rootDir = path.dirname(path.resolve(migrationFilePath))
   const pathKeys = Object.keys(migrationConfig.paths)
   for (var i = 0; i < pathKeys.length; i++) {
@@ -58,9 +58,9 @@ function resolvePaths(migrationConfig, migrationFilePath) {
   return migrationConfig
 }
 
-function unresolvePaths(migrationConfig, migrationFilePath) {
-  const newConfig =  Object.assign({}, migrationConfig)
- 
+function unresolvePaths (migrationConfig, migrationFilePath) {
+  const newConfig = Object.assign({}, migrationConfig)
+
   const rootDir = path.dirname(path.resolve(migrationFilePath))
   const pathKeys = Object.keys(migrationConfig.paths)
   for (var i = 0; i < pathKeys.length; i++) {
